77a1cb57ea13ef2599677beee77e53b9f8cc5075
Allen Piscitello <allen.piscitello@gmail.com>
2018-12-27 15:28:01 -0600

    Adding list of assets

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+import sqlite3
 
 app = Flask(__name__)
 
+with open("config.json") as configfile:
+    config = json.load(configfile)
 
 @app.route("/")
 def view_assets():
-    return "all assets"
+    db = sqlite3.connect(config["database"])
+    assets = db.execute("SELECT asset FROM issuances WHERE token IS NOT NULL ORDER BY datetime DESC")
+    return render_template("all_assets.jinja", assets=assets)
 
 @app.route("/asset/<string:id>")
 def view_asset(id):
