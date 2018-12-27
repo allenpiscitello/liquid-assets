9d2a788ce820a725c87bcd93046cd06cbd83b79a
Allen Piscitello <allen.piscitello@gmail.com>
2018-12-27 12:55:57 -0600

    Adding asset list

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def view_assets():
+    return "all assets"
+
 @app.route("/asset/<string:id>")
 def view_asset(id):
     return "hello"  + id
