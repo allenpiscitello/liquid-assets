8dde2fe11f1b92b27e422db7180c7da4ba731e60
Allen Piscitello <allen.piscitello@gmail.com>
2018-12-28 08:47:16 -0600

    Displaying asset details and adding links

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ with open("config.json") as configfile:
 @app.route("/")
 def view_assets():
     db = sqlite3.connect(config["database"])
-    assets = db.execute("SELECT asset FROM issuances WHERE token IS NOT NULL ORDER BY datetime DESC")
+    assets = db.execute("SELECT asset FROM issuances WHERE token IS NOT NULL ORDER BY datetime ASC")
     return render_template("all_assets.jinja", assets=assets)
 
 @app.route("/asset/<string:id>")
 def view_asset(id):
-    return "hello"  + id
+    db = sqlite3.connect(config["database"])
+    #get reissuance token id
+    token_info = db.execute("SELECT token, tokenamount FROM issuances WHERE asset=? AND token IS NOT NULL", (id, )).fetchone()
+    assets = db.execute("SELECT block, datetime, amount FROM issuances WHERE asset=? ORDER BY datetime ASC", (id, ))
+    reissuances = db.execute("SELECT block, datetime, amount FROM issuances WHERE asset=?", (token_info[0], ))
+    return render_template("asset_details.jinja", asset_transactions = assets, reissuances=reissuances, token_info=token_info, asset_id=id)
